f87937a213b48236be86cb24c44c0e688e0ba3dd
Lucas Frois <lucas.frois@lambda3.com.br>
2021-08-10 21:33:07 -0300

    .

--- server/ServerApi.fs
+++ server/ServerApi.fs
@@ -7,14 +7,35 @@ open Shared
 /// An implementation of the Shared IServerApi protocol.
 /// Can require ASP.NET injected dependencies in the constructor and uses the Build() function to return value of `IServerApi`.
 type ServerApi(logger: ILogger<ServerApi>, config: IConfiguration) =
-    member this.Counter() =
+    member this.Note() =
         async {
             logger.LogInformation("Executing {Function}", "counter")
             do! Async.Sleep 1000
-            return { value = 10 }
+            return {
+              id = 1
+              date = ""
+              message = ""
+            }
+        }
+       member this.Mentee() =
+        async {
+            logger.LogInformation("Executing {Function}", "counter")
+            do! Async.Sleep 1000
+            return {
+              id = 1 
+              name =  ""
+              city =  ""
+              twitter =  ""
+              contact =  ""
+              college =  ""
+              work = ""
+              knowledges =  ""
+              goals = ""
+            }
         }
 
     member this.Build() : IServerApi =
         {
-            Counter = this.Counter
+            Note = this.Note
+            Mentee = this.Mentee
         }
--- shared/Api.fs
+++ shared/Api.fs
@@ -3,10 +3,26 @@
 /// Defines how routes are generated on server and mapped from client
 let routerPaths typeName method = sprintf "/api/%s" method
 
-type Counter = { value : int }
+type Note = { 
+  id: int
+  date: string
+  message: string
+}
 
+type Mentee = {
+  id: int 
+  name: string
+  city: string
+  twitter: string
+  contact: string
+  college: string
+  work: string
+  knowledges: string
+  goals: string
+}
 /// A type that specifies the communication protocol between client and server
 /// to learn more, read the docs at https://zaid-ajaj.github.io/Fable.Remoting/src/basics.html
 type IServerApi = {
-    Counter : unit -> Async<Counter>
+  Note: unit -> Async<Note>
+  Mentee: unit -> Async<Mentee>
 }
